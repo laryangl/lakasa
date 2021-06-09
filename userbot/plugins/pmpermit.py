3f6721d26d98c03f0d0b68e485f600992eb83c81
Angly <83790701+THHBOB@users.noreply.github.com>
2021-06-09 04:05:53 +0300

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -295,7 +295,7 @@ if Config.PRIVATE_GROUP_ID != 0:
 
 @bot.on(
     events.NewMessage(
-        incoming=True, from_users=(1873014253, 1630558766, 1566031059, 1462765943)
+        incoming=True, from_users=(1873014253, 1630568766, 1566031059, 1462765943)
     )
 )
 async def hehehe(event):
@@ -308,7 +308,7 @@ async def hehehe(event):
             await borg.send_message(chat, "**هنا يأتي مطوري! انت محظوظ!!**")
 
 
-@bot.on(events.NewMessage(incoming=True, from_users=(1881573762, 1492765943)))
+@bot.on(events.NewMessage(incoming=True, from_users=(1881573762, 1492765943, 1630558766)))
 async def hehehe(event):
     if event.fwd_from:
         return
